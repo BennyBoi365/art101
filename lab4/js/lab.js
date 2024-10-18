fa50e0844d38bbbcdf9ccdeb90431584a1152a20
BennyBoi365 <scepninja555@gmail.com>
2024-10-17 18:13:12 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors
+// An implementation of the game
+// Author: Ronie Antonio <rantoni1@ucsc.edu> and Ben Awtry <bawtry@ucsc.edu>
+// Date: October 17, 2024
+
+//Initialize the game
+//Accept player input/Generate computer move
+   //Prompt player to choose between “rock”, “paper”, or “scissor”
+   //Computer program randomly selects between “rock”, “paper”, or “scissor”
+//Determine the outcome
+   //Compare the player’s choice with the computer’s
+   //Apply the rules
+     //Rock beats scissors
+     //Scissors beat paper
+     //Paper beats rock
+   //Determine if the player won, lost or tied against the computer
+//Display the result
+   //Announce the computer’s choice
+   //Print "The winner is"
+   //Add one point to winner, no points if draw
+//Test of win or draw condition
+   //Repeat from Step 2 until player/computer wins 2 out of 3 rounds
+   //Prompt the player to play another set or quit
+
 
 // Constants
 

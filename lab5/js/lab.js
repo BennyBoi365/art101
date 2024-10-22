b922978a855adecf69a6151f99e0a7a3e7d93e80
BennyBoi365 <scepninja555@gmail.com>
2024-10-21 19:50:38 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 js code
+/*
+ * Author: Ronie Antonio and Ben Awtry
+ * Emails: <rantoni@ucsc.edu> and <bawtry@ucsc.edu>
+ * Created: 20 Oct 2024
+ * Liscence: Public Domain
+*/
+// Defining Variables
+make = "Toyota";
+model = "Camry";
+color = "Blue";
+year = 2010;
 
-// Constants
+// Calculating Age of Car
+age = 2024 - year;
 
-// Functions
+// Will it output?
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
